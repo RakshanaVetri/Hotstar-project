424a54a5bfb91a4595bf6ea218d9c70b5ec458f8
RakshanaVetri <154620731+RakshanaVetri@users.noreply.github.com>
2024-06-03 11:37:27 +0530

    Update backend.tf

--- eks_terraform/backend.tf
+++ eks_terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "deep-coders" # Replace with your actual S3 bucket name
+    bucket = "myawshotspot" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
